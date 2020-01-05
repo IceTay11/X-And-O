0ebab84c45709e5953a6b829746de26833adda3c
Tay-yib Amod <Tay.amod@gmail.com>
2020-01-05 23:15:35 +0200

    X and O

---
+++ XandO/XandO/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XandO
+{
+	public partial class MainPage : ContentPage
+	{
+        private Button[] buttons = new Button[9];
+        private XandOGameEngine game = new XandOGameEngine();
+		public MainPage()
+		{
+			InitializeComponent();
+
+            buttons[0] = button1;
+            buttons[1] = button2;
+            buttons[2] = button3;
+            buttons[3] = button4;
+            buttons[4] = button5;
+            buttons[5] = button6;
+            buttons[6] = button7;
+            buttons[7] = button8;
+            buttons[8] = button9;
+            
+        }
+        
+
+        public void button_Clicked(object sender, EventArgs e)
+        {
+            game.SetButton((Button)sender);
+
+            if (game.CheckWinner(buttons))
+            {
+                GameOverStackLayout.IsVisible = true;
+            }
+        }
+
+
+        public void play_Clicked(object sender, EventArgs e)
+        {
+            game.ResetGame(buttons);
+            GameOverStackLayout.IsVisible = false;
+        }
+	}
+}
